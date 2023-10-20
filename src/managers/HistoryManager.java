afc5727a074996dfb0a13fd0096a1378dcc9ec1b
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2023-10-20 13:32:54 +0300

    Созданы основные методы (7 задач) в библиотеке

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -8,7 +8,6 @@ package managers;
 import entity.Book;
 import entity.History;
 import entity.Reader;
-import java.util.Arrays;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
@@ -19,9 +18,13 @@ import java.util.Scanner;
  */
 public class HistoryManager{
     private Scanner scanner;
+    private ReaderManager readerManager;
+    private BookManager bookManager;
     
-    public HistoryManager(Scanner scanner) {
+    public HistoryManager(Scanner scanner,BookManager bookManager, ReaderManager readerManager) {
         this.scanner = scanner;
+        this.readerManager = readerManager;
+        this.bookManager = bookManager;
     }
     public History giveOutBook(Book[] books, Reader[] readers){
         History history = new History();
@@ -32,22 +35,24 @@ public class HistoryManager{
          *  4. сделать 1-3 пункт для книги
          *  5. добавить в history дату выдачи книги (текущую дату)
          */
-        System.out.println("List readers: ");
-        for (int i = 0; i < readers.length; i++) {
-            System.out.printf("%d. %s %s %s%n",i+1,readers[i].getFirstname()
-                    ,readers[i].getLastname(),readers[i].getPhone());
-        }
+        readerManager.printListReaders(readers);
         int selectedReaderNumber = scanner.nextInt(); scanner.nextLine();
         history.setReader(readers[selectedReaderNumber-1]);
-        System.out.println("List books: ");
-        for (int i = 0; i < books.length; i++) {
-            System.out.printf("%d. %s. %d. %s%n",i+1,books[i].getTitle()
-                    ,books[i].getPublishedYear(),Arrays.toString(books[i].getAuthors()));
-        }
+        bookManager.printListBooks(books);
         int selectedBookNumber = scanner.nextInt(); scanner.nextLine();
         history.setBook(books[selectedBookNumber-1]);
         history.setDateOnHand(new GregorianCalendar().getTime());
         return history;
+    }
+
+    public void returnBook(History[] histories) {
+        bookManager.printListGiveOutBooks(histories);
+        System.out.print("Select book for return: ");
+        int historyNumber = scanner.nextInt(); scanner.nextLine();
+        histories[historyNumber-1].setDateBack(new GregorianCalendar().getTime());
+        System.out.printf("Book \"%s\" returned%n", 
+                histories[historyNumber-1].getBook().getTitle()
+        );
     }
     
 }
